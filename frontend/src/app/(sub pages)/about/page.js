2c5703716ef4be8c0c8d3388577c0284986a07d9
KushalJetty <kushaljetty1203@gmail.com>
2026-03-21 18:05:28 +0530

    feat: straw hat added

--- frontend/src/app/(sub pages)/about/page.js
+++ frontend/src/app/(sub pages)/about/page.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
 import bg from "../../../../public/background/about-background.png";
 import RenderModel from "@/components/RenderModel";
-// import HatModel from "@/components/models/HatModel";
+// import StrawHat from "@/components/models/Straw-hat";
 import AboutDetails from "@/components/about";
 import dynamic from "next/dynamic";
-const HatModel = dynamic(() => import("@/components/models/HatModel"), {
+const StrawHat = dynamic(() => import("@/components/models/Straw-hat"), {
   ssr: false,
 });
 
@@ -25,12 +25,12 @@ export default function Home() {
 
       <div className="w-full h-3/5 xs:h-3/4 sm:h-screen absolute top-1/2 -translate-y-1/2 left-0 z-10">
         <RenderModel>
-          <HatModel />
+          <StrawHat />
         </RenderModel>
       </div>
 
       <div className="relative w-full h-screen flex flex-col items-center justify-center">
-        <div className="absolute flex flex-col items-center text-center top-1/2 sm:top-[60%] left-1/2 -translate-y-1/2 -translate-x-1/2">
+        <div className="absolute flex flex-col items-center text-center top-[65%] sm:top-[75%] left-1/2 -translate-y-1/2 -translate-x-1/2">
           <h1 className="font-bold  text-6xl xs:text-7xl sm:text-8xl  lg:text-9xl text-accent">
             Kushal Jetty
           </h1>
