d3642d79e60bb8445df6468f295315487e45583f
KushalJetty <kushaljetty1203@gmail.com>
2026-03-22 03:58:14 +0530

    changes

--- frontend/src/components/about/index.jsx
+++ frontend/src/components/about/index.jsx
@@ -47,7 +47,7 @@ const AboutDetails = () => {
         >
           <img
             className="w-full h-auto"
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api/top-langs?username=KushalJetty&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            src={`https://github-readme-stats.vercel.app/api/top-langs?username=KushalJetty&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
             alt="Kushal Jetty"
             loading="lazy"
           />
@@ -56,7 +56,7 @@ const AboutDetails = () => {
         <ItemLayout className={"col-span-full md:col-span-8 !p-0"}>
           <img
             className="w-full h-auto"
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STATS_URL}/api?username=KushalJetty&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
+            src={`https://github-readme-stats.vercel.app/api?username=KushalJetty&theme=transparent&hide_border=true&title_color=FEFE5B&text_color=FFFFFF&icon_color=FEFE5B&text_bold=false`}
             alt="Kushal Jetty"
             loading="lazy"
           />
@@ -74,7 +74,7 @@ const AboutDetails = () => {
         <ItemLayout className={"col-span-full md:col-span-6 !p-0"}>
           <img
             className="w-full h-auto"
-            src={`${process.env.NEXT_PUBLIC_GITHUB_STREAK_STATS_URL}?user=KushalJetty&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
+            src={`https://github-readme-streak-stats.herokuapp.com?user=KushalJetty&theme=dark&hide_border=true&type=svg&background=EB545400&ring=FEFE5B&currStreakLabel=FEFE5B`}
             alt="Kushal Jetty"
             loading="lazy"
           />
