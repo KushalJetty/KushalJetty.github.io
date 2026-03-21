e409d9dcff094047603b39d36b6036a6fa537873
KushalJetty <kushaljetty1203@gmail.com>
2026-03-21 15:02:57 +0530

    fix: katana added

--- frontend/src/app/(sub pages)/projects/page.js
+++ frontend/src/app/(sub pages)/projects/page.js
@@ -3,10 +3,10 @@ import bg from "../../../../public/background/projects-background.png";
 import ProjectList from "@/components/projects";
 import { projectsData } from "../../data";
 import RenderModel from "@/components/RenderModel";
-// import Staff from "@/components/models/Staff";
+// import Katana from "@/components/models/katana";
 import dynamic from "next/dynamic";
 
-const Staff = dynamic(() => import("@/components/models/Staff"), {
+const Katana = dynamic(() => import("@/components/models/katana"), {
   ssr: false,
 });
 
@@ -27,9 +27,9 @@ export default function Home() {
 
       <ProjectList projects={projectsData} />
 
-      <div className="flex items-center justify-center fixed  top-16  lg:top-20 -translate-x-1/2 lg:translate-x-0 -z-10 left-1/2 lg:-left-24 h-screen">
+      <div className="flex items-center justify-center fixed top-0 -translate-x-1/2 lg:translate-x-0 -z-10 left-1/2 lg:-left-24 h-screen">
         <RenderModel>
-          <Staff />
+          <Katana />
         </RenderModel>
       </div>
     </>
